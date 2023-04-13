803c022243f852c9eee719fba4623d9592d0bd07
Nicolas Aguirre <nico03aguirre@gmail.com>
2023-04-13 07:12:49 -0600

    Primera prueba de vista

---
+++ Futbol/Controllers/MantenedorController.cs
@@ -0,0 +1,33 @@
+﻿using Futbol.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Futbol.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        
+        public async Task<IActionResult> Listar()
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:5079/api/equipos");
+            var equipos = await response.Content.ReadFromJsonAsync<List<Equipo>>();
+            return View(equipos);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        public IActionResult Actualizar()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
